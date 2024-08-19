03d8f8f3fe2cf6b9a4fbbcf40031db4156f5f6dd
codewithDOX <166832988+codewithDOX@users.noreply.github.com>
2024-08-19 06:25:18 +0000

    datatypes

---
+++ lesson.js/datatypes-ecmaStandards.js
@@ -0,0 +1,33 @@
+ //=======//ECMA//==========//
+
+ // ECMA script is an international organization for Javascript who sets standards for the language so you can visit there webiste --   https://tc39.es  and read documentation although recommended for extreme professionals so it's better to prefer MDN references //
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js, not browser
+
+//  console.log( 3 + 3 ); console.log("Ron")// DO not write the code in this manner it destroys the readability , documentation and representation of your code though it will still run // 
+
+
+//You can excute this even without a semicolon but it's a good practice , And use an extension called prettier to make your code much more readable// 
+
+console.log( 3 + 3 )
+console.log("Ron")
+
+//=================================================//
+
+//========DATA-TYPES (primitive's)=======//
+
+let name = "Ace Idris Qwentana" //string datatypes - the string data type represents a sequence of characters enclosed in single quotes, double quotes, or backticks.
+
+let age = "19" // Integer types -In JavaScript, you can use let to define a variable, and the type of the variable is determined dynamically based on the value assigned to it. //
+
+
+let isLoggedIn = false // Boolean - either true or flase //
+
+// Range of a number => 2  superscript 53 
+// bigint for long number
+//null => standalone value it means its argument is empty OR "When a variable is assigned null, it means the variable is intentionally set to have an empty or non-existent value."
+ 
+// undefined => when no values are provided to variable 
+// symbol => unique 
