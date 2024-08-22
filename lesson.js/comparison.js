a6ed36ca6d37d6bd652719e72d4ca6a09233288b
codewithDOX <166832988+codewithDOX@users.noreply.github.com>
2024-08-22 17:11:23 +0000

    comparison

---
+++ lesson.js/comparison.js
@@ -0,0 +1,24 @@
+
+//================COMPARISON==================//
+ console.log( 2 >1);
+ console.log( 2 >= 1 );
+ console.log( 2 < 1);
+ console.log( 2 == 1 );
+ console.log( 2 != 1);
+
+ // diffrenet datatypes
+
+ console.log ( "2" > 1 );
+ console.log("02 > 1 ");
+
+ // Do not perform these type of comparison
+ 
+console.log( null > 0); // here null is an empty value , so the output is false 
+console.log( null == 0); // here null is == to zero 
+console.log( null >= 0); // And that's why because the null has now assigned as zero it gives us the output ture , considering "="
+
+
+// "==" and "==="//
+console.log( "2" == 2 ); // okay it's a comparison
+console.log( "2" === 2); // strict check --> It strictly checks whether the comparison has made is done under the same datatypes 
+ 
